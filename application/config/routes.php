99a870c908cfd5c0b5f85022bb7dfc40d8924db8
naokity <naomiwatabetaylor@gmail.com>
2015-04-23 10:21:43 -0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,7 +55,13 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
+
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 */
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
