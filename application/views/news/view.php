6cbca7df71f81adfa4a133fc7913dc6de0e5e28a
naokity <naomiwatabetaylor@gmail.com>
2015-04-23 10:33:37 -0700

    Update view.php

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,3 +1,7 @@
 <?php
 echo '<h2>'.$news_item['title'].'</h2>';
 echo $news_item['text'];
+?>
+
+<p><a href="http://mykhabarovsk.com/ci/index.php/news/create"</a>Go back add new item</p>
+<p><a href="http://mykhabarovsk.com/ci/index.php/news"</a>Go back News Archive</p>
