cd9f58c0986fdc6d14fadaa555db736e7e4ffc91
asd <wdawd>
2015-04-23 15:47:13 -0700

    Added three files necessary for the application
    Added:
    create.php - creates new news article
    success.php - success page once article added

---
+++ application/views/news/success.php
@@ -0,0 +1,3 @@
+<p>You are successful log in</p>
+<p><a href="http://mykhabarovsk.com/ci/index.php/news/create"</a>Go back add new item</p>
+<p><a href="http://mykhabarovsk.com/ci/index.php/news"</a>Go back News Archive</p>
